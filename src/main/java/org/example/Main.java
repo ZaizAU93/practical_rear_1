2533aea69f44053739d6c1dcc6253a59e3b1bd06
ZaizAU93 <zajtz.andrey1993@mail.ru>
2023-08-28 20:41:09 +0300

    практичесое задание №1,2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+
+        int age = 29;
+
+        System.out.println("Зайцев Андрей"+ age);
+
+    }
+
+
+}
